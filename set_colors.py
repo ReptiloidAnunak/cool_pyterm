53b216262b847e2acce5920ee484de9080aa4f16
ReptiloidAnunak <amanitocornholio@proton.me>
2024-06-15 01:23:36 -0300

    initial commit

---
+++ set_colors.py
@@ -0,0 +1,17 @@
+from colorama import Back, Fore, Style, init
+# init(autoreset=True)
+
+
+def terminal_set_color_block(name_function: str,
+                        set_all_str_color=True):
+    if set_all_str_color:
+        init(autoreset=False)
+        name_function = Back.YELLOW + Fore.BLACK + name_function + Style.RESET_ALL
+    else:
+        name_function = Back.YELLOW + Fore.BLACK + name_function
+    print(name_function)
+
+
+terminal_set_color_block('ДЖИГУРДА', set_all_str_color=False)
+print("фацфафцацфа")
+print("хуйпизда")
